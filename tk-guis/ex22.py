9728d258ae2fde3bfc17a78b6f7ba9da2fa40c98
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-21 14:20:26 +0100

    no fill

---
+++ tk-guis/ex22.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("My tkinter App")
+root.geometry("800x600")
+
+rect_1 = tk.Label(root, text="Rectangle 1", bg="green", fg="white")
+rect_1.pack(ipadx=10, ipady=10, fill="both", side="top", expand=True)
+
+rect_2 = tk.Label(root, text="Rectangle 2", bg="red", fg="white")
+rect_2.pack(ipadx=10, ipady=10, side="top", expand=True)
+
+root.mainloop()
