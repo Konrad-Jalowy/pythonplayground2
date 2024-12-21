5eeaf592f1d6d6ee215c73fe61d4766102a79a4d
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-21 12:50:23 +0100

    Ex9

---
+++ tk-guis/ex9.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+
+def greet():
+    print("Hello world")
+
+root = tk.Tk()
+root.title("My tkinter App")
+root.geometry("800x600")
+
+
+greet_btn = ttk.Button(root, text="Greet", command=greet)
+greet_btn.pack(side="left")
+
+exit_btn = ttk.Button(root, text="Exit", command=root.destroy)
+exit_btn.pack(side="right")
+
+root.mainloop()
