d23d76fabf3a9ab6e81bca5e223874438c612ad7
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 11:00:23 +0100

    were getting somewhere

--- tk-guis/ex28.py
+++ tk-guis/ex28.py
@@ -9,7 +9,10 @@ last_name = tk.StringVar()
 user_age = tk.StringVar()
 
 first_label = ttk.Label(root, text="First Name:")
+first_entry = ttk.Entry(root, width=15, textvariable=first_name)
 last_label = ttk.Label(root, text="Last Name:")
+last_entry = ttk.Entry(root, width=15, textvariable=last_name)
 age_label = ttk.Label(root, text="First Name:")
+age_entry = ttk.Entry(root, width=15, textvariable=user_age)
 
 root.mainloop()
