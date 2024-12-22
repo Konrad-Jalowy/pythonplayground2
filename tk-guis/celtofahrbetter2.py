4cfead05832d2b2bbc13f33ece3dbc8af03e9435
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 16:47:23 +0100

    even better

---
+++ tk-guis/celtofahrbetter2.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import ttk
+import tkinter.font as tkFont
+
+def on_input_change(*args):
+    converted_text.set(f"")
+
+def callback(P):
+    if str.isdigit(P) or P == "":
+        return True
+    else:
+        return False
+
+def cel_to_fahr(cel):
+    return (cel * 1.8) + 32
+
+def click_handler():
+    _cel = float(cel_text.get())
+    _fahr = cel_to_fahr(_cel)
+    converted_text.set(f"{_fahr}")
+
+
+root = tk.Tk()
+root.title("Cel to Fahr Converter")
+root.geometry("800x600")
+root.resizable(False, False)
+
+vcmd = root.register(callback)
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+fontApp = tkFont.Font(size=16)
+
+
+cel_text = tk.StringVar(value="")
+cel_text.trace_add("write", on_input_change)
+converted_text = tk.StringVar(value="")
+
+main_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
+main_frame.grid(row=0, column=0)
+
+input_label = ttk.Label(main_frame, text="Cel:", font=fontApp)
+input_label.grid(row=0, column=0, padx=(0, 10))
+
+input_entry = ttk.Entry(main_frame, textvariable=cel_text,  validate='all', validatecommand=(vcmd, '%P'))
+input_entry.grid(row=0, column=1, padx=(0, 10), ipady=5)
+
+output_label = ttk.Label(main_frame, text="Fahr:", font=fontApp)
+output_label.grid(row=1, column=0, padx=(0, 10))
+
+output_entry = ttk.Entry(main_frame, textvariable=converted_text, state="readonly")
+output_entry.grid(row=1, column=1, padx=(0, 10), ipady=5)
+
+btn = tk.Button(main_frame, text="Convert", font=fontApp, command=click_handler)
+btn.grid(row=2, column=0, columnspan=2, padx=(0, 10), sticky="ew")
+
+root.mainloop()
