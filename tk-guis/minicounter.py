b0b20c6aa11c53176f2d7da0404e7a61011c8b9a
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 14:08:16 +0100

    were getting somewhere

--- tk-guis/minicounter.py
+++ tk-guis/minicounter.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 import tkinter.font as tkFont
 
+def clickHandler():
+    current_sum = int(clicked.get())
+    current_sum += 1
+    print(current_sum)
+    
 root = tk.Tk()
 root.title("Greeter")
 root.geometry("800x600")
@@ -17,10 +22,12 @@ main.grid(row=0, column=0)
 
 clicked = tk.StringVar(value=0)
 
+
 first_label = ttk.Label(main, text="Clicks: 0", font=fontApp)
 first_label.grid(row=0, column=0, padx=(0, 10))
 
-age_label = tk.Button(main, text="Click me", command= lambda : print("clicked"), font=fontApp)
+age_label = tk.Button(main, text="Click me", command=clickHandler, font=fontApp)
 age_label.grid(row=1, column=0, padx=(0, 10))
 
 root.mainloop()
+
