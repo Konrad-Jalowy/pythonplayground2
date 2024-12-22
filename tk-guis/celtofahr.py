5a6830c3d0d9403bc84d9276eb274e4238e4546d
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 14:32:27 +0100

    were getting somewhere

--- tk-guis/celtofahr.py
+++ tk-guis/celtofahr.py
@@ -4,3 +4,11 @@ import tkinter.font as tkFont
 
 def cel_to_fahr(cel):
     return (cel * 1.8) + 32
+
+
+root = tk.Tk()
+root.title("Cel to Fahr Converter")
+root.geometry("800x600")
+root.resizable(False, False)
+
+root.mainloop()
