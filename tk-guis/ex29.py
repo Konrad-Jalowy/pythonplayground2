636c450befab351cdf443d6f12a0623e5df5a43e
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 11:06:38 +0100

    column and row weight

---
+++ tk-guis/ex29.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Greeter")
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=1)
+root.rowconfigure(2, weight=1)
+
+first_name = tk.StringVar()
+last_name = tk.StringVar()
+user_age = tk.StringVar()
+
+first_label = ttk.Label(root, text="First Name:").grid(row=0, column=0, padx=(0, 10))
+first_entry = ttk.Entry(root, width=15, textvariable=first_name).grid(row=0, column=1, padx=(0, 10))
+
+last_label = ttk.Label(root, text="Last Name:").grid(row=1, column=0, padx=(0, 10))
+last_entry = ttk.Entry(root, width=15, textvariable=last_name).grid(row=1, column=1, padx=(0, 10))
+
+age_label = ttk.Label(root, text="First Name:").grid(row=2, column=0, padx=(0, 10))
+age_entry = ttk.Entry(root, width=15, textvariable=user_age).grid(row=2, column=1, padx=(0, 10))
+
+root.mainloop()
