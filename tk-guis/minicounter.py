f1a458ccbc95e444504c81b13374dcce8b88000d
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 14:13:04 +0100

    clicker

--- tk-guis/minicounter.py
+++ tk-guis/minicounter.py
@@ -3,10 +3,10 @@ from tkinter import ttk
 import tkinter.font as tkFont
 
 def clickHandler():
-    current_sum = int(clicked.get())
-    current_sum += 1
-    print(current_sum)
-    clicked.set(current_sum)
+    click_number = int(clicked.get())
+    click_number += 1
+    clicked.set(click_number)
+    click_label.configure(text=f"Clicks: {click_number}")
 
 root = tk.Tk()
 root.title("Greeter")
@@ -24,11 +24,11 @@ main.grid(row=0, column=0)
 clicked = tk.StringVar(value=0)
 
 
-first_label = ttk.Label(main, text="Clicks: 0", font=fontApp)
-first_label.grid(row=0, column=0, padx=(0, 10))
+click_label = ttk.Label(main, text="Clicks: 0", font=fontApp)
+click_label.grid(row=0, column=0, padx=(0, 10))
 
-age_label = tk.Button(main, text="Click me", command=clickHandler, font=fontApp)
-age_label.grid(row=1, column=0, padx=(0, 10))
+btn = tk.Button(main, text="Click me", command=clickHandler, font=fontApp)
+btn.grid(row=1, column=0, padx=(0, 10))
 
 root.mainloop()
 
