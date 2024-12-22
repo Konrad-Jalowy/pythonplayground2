90709aba0c9a0dd5563c0d85066b1c486d857acf
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 15:21:18 +0100

    validation of input

--- tk-guis/validateentry.py
+++ tk-guis/validateentry.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 
-
+def callback(P):
+    if str.isdigit(P) or P == "":
+        return True
+    else:
+        return False
 
 
 root = tk.Tk()
@@ -9,7 +13,9 @@ root.title("Validate ipt")
 root.geometry("800x600")
 root.resizable(False, False)
 
-some_entry = ttk.Entry(root)
+vcmd = root.register(callback)
+
+some_entry = ttk.Entry(root, validate='all', validatecommand=(vcmd, '%P'))
 some_entry.pack(side="left", expand=True, fill="x")
 
 root.mainloop()
