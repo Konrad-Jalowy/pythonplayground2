e31f24c97c6d2f8f99a2ab5205dee48e03413738
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 11:13:02 +0100

    sticky

---
+++ tk-guis/ex30.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Greeter")
+
+root.columnconfigure(1, weight=1)
+
+first_name = tk.StringVar()
+last_name = tk.StringVar()
+user_age = tk.StringVar()
+
+first_label = ttk.Label(root, text="First Name:").grid(row=0, column=0, padx=(0, 10))
+first_entry = ttk.Entry(root, width=15, textvariable=first_name).grid(row=0, column=1, padx=(0, 10), sticky="we")
+
+last_label = ttk.Label(root, text="Last Name:").grid(row=1, column=0, padx=(0, 10))
+last_entry = ttk.Entry(root, width=15, textvariable=last_name).grid(row=1, column=1, padx=(0, 10), sticky="we")
+
+age_label = ttk.Label(root, text="First Name:").grid(row=2, column=0, padx=(0, 10))
+age_entry = ttk.Entry(root, width=15, textvariable=user_age).grid(row=2, column=1, padx=(0, 10), sticky="we")
+
+root.mainloop()
