81103b3c494a86a5cd3cd6fbbf523b99c8c91317
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-22 14:44:21 +0100

    were getting somewhere

--- tk-guis/celtofahr.py
+++ tk-guis/celtofahr.py
@@ -17,12 +17,15 @@ root.rowconfigure(0, weight=1)
 fontApp = tkFont.Font(size=16)
 
 cel = tk.DoubleVar(value=0)
-converted_text = tk.StringVar(value="Enter value in Celsius and convert do Fahr")
+converted_text = tk.StringVar(value="")
 
 main_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
 main_frame.grid(row=0, column=0)
 
-output_entry = ttk.Entry(main_frame, width=15, textvariable=converted_text)
-output_entry.grid(row=0, column=0, padx=(0, 10), ipady=5, sticky="we")
+output_label = ttk.Label(main_frame, text="Fahr:", font=fontApp)
+output_label.grid(row=0, column=0, padx=(0, 10))
+
+output_entry = ttk.Entry(main_frame, textvariable=converted_text)
+output_entry.grid(row=0, column=1, padx=(0, 10), ipady=5)
 
 root.mainloop()
