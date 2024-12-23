0a152b9ea7e8d995084a92df90e102d75ddd69e5
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-23 12:57:32 +0100

    done

--- tk-guis/ex47.py
+++ tk-guis/ex47.py
@@ -9,8 +9,11 @@ notebook = ttk.Notebook(root)
 notebook.pack(expand=True, fill='both')
 
 frame1 = tk.Frame(notebook)
+ttk.Label(frame1, text="Frame 1").pack()
 frame2 = tk.Frame(notebook)
+ttk.Label(frame2, text="Frame 2").pack()
 frame3 = tk.Frame(notebook)
+ttk.Label(frame3, text="Frame 3").pack()
 
 frame1.pack()
 frame2.pack()
