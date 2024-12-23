e59a1849fa1d39cf01a5b39f7a71f5403fedc533
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-23 11:29:29 +0100

    were getting somewhere

---
+++ tk-guis/ex43.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def clickhandler():
+    frame_1.tkraise()
+
+root = tk.Tk()
+root.title("App")
+
+root.columnconfigure(0, weight=1)
+
+frame_1 = ttk.Frame(root, padding=(30, 15))
+frame_1.grid(column=0, row=0)  # column=0 row=0 by default
+
+frame_2 = ttk.Frame(root, padding=(30, 15))
+frame_2.grid(column=0, row=0)  # column=0 row=0 by default
+
+btns = ttk.Frame(root, padding=(30, 15))
+btns.grid(column=0, row=1)  # column=0 row=0 by default
+
+# -- Widgets --
+
+frame1_label = ttk.Label(frame_1, text="Frame1")
+frame2_label = ttk.Label(frame_2, text="Frame2")
+btn = ttk.Button(btns, text="Switch frame", command=clickhandler)
+btn.grid(columnspan=2)
+
+frame1_label.grid()
+frame2_label.grid()
+
+root.mainloop()
