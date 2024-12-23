d239d9c81d589e059edd843776a5eaae9e588d1f
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-23 09:50:20 +0100

    file path

---
+++ 101/filepath1.py
@@ -0,0 +1,7 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+cwd = os.getcwd()
+
+print(__file__) #file path
+print(dir_path) #file directory path
+print(cwd) # path from which we run the python command 
