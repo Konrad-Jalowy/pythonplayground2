ee9c556f8394ff9bf08bd37c9dd37880ffa32fb1
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-23 13:18:08 +0100

    were getting somewhere

---
+++ tk-guis/ex48.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk 
+from tkinter import filedialog as fd
+
+root = tk.Tk()
+root.title("File Dialog App")
+root.geometry("800x600")
+
+main_frame = tk.Frame(root)
+main_frame.grid()
+
+root.mainloop()
