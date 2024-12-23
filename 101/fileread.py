73effbe51df8a4b0293645ae62522d6be3a0b9a3
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-23 10:00:23 +0100

    were getting somewhere

--- 101/fileread.py
+++ 101/fileread.py
@@ -1,3 +1,10 @@
 import os
 path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test.txt")
 print(path)
+
+fh = open(path, "r")
+lines = fh.readlines()
+fh.close()
+
+for line in lines:
+    print(line)
