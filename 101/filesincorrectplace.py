9d7d7f3e49b21c5c702c49d53b2efb0bb1fb2be9
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-23 09:56:02 +0100

    were getting somewhere

--- 101/filesincorrectplace.py
+++ 101/filesincorrectplace.py
@@ -1,3 +1,12 @@
 import os
 path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test.txt")
 print(path)
+
+fh = open(path, "w")
+fh.write("hello world 1")
+fh.write("hello world 2")
+fh.close()
+
+fh = open(path, "a")
+fh.write("hello world 3")
+fh.close()
