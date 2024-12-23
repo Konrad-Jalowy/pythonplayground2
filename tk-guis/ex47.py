4c0cd4e7adfaa67c177bf9f27d6d9c90c37146c8
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-23 12:56:13 +0100

    ex47

--- tk-guis/ex47.py
+++ tk-guis/ex47.py
@@ -7,4 +7,17 @@ root.geometry("800x600")
 
 notebook = ttk.Notebook(root)
 notebook.pack(expand=True, fill='both')
+
+frame1 = tk.Frame(notebook)
+frame2 = tk.Frame(notebook)
+frame3 = tk.Frame(notebook)
+
+frame1.pack()
+frame2.pack()
+frame3.pack()
+
+notebook.add(frame1, text="Tab 1")
+notebook.add(frame2, text="Tab 2")
+notebook.add(frame3, text="Tab 3")
+
 root.mainloop()
