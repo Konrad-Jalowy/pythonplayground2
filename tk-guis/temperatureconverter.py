9da49e1952690df1734a5fc8ef15e693cd6bc1f6
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-24 13:50:01 +0100

    were getting somewhere

--- tk-guis/temperatureconverter.py
+++ tk-guis/temperatureconverter.py
@@ -62,6 +62,51 @@ class CelToFahrFrame(ttk.Frame):
             return True
         else:
             return False
+
+class FahrToCelFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        self.vcmd = self.register(self.input_callback)
+
+        self.fahr_text = tk.StringVar(value="")
+        self.fahr_text.trace_add("write", self.on_input_change)
+        self.converted_text = tk.StringVar(value="")
+
+        self.label_input = ttk.Label(self, text="Temperature in Fahr: ")
+        self.entry_input = ttk.Entry(self, textvariable=self.fahr_text, validate='all', validatecommand=(self.vcmd, '%P'))
+
+        self.label_output = ttk.Label(self, text="Temperature in Cel: ")
+        self.entry_output = ttk.Entry(self, textvariable=self.converted_text, state="readonly")
+        
+        self.btn = tk.Button(self, text="Convert", command=self.clickhandler)
+
+        self.label_input.grid(row=0, column=0, sticky="W")
+        self.entry_input.grid(row=0, column=1)
+
+        self.label_output.grid(row=1, column=0, sticky="W")
+        self.entry_output.grid(row=1, column=1)
+
+        self.btn.grid(row=2, column=0, columnspan=2, sticky="ew")
     
+    def clickhandler(self):
+        if self.fahr_text.get() == "":
+            return
+        _fahr = float(self.fahr_text.get())
+        _cel = self.fahr_to_cel(_fahr)
+        self.converted_text.set(f"{_cel}")
+
+    def fahr_to_cel(self, fahrenheit): 
+        celsius = (fahrenheit - 32) * 5 / 9 
+        return celsius 
+
+    def on_input_change(self, *args):
+        self.converted_text.set(f"")
+
+    def input_callback(self, P):
+        if str.isdigit(P) or P == "":
+            return True
+        else:
+            return False
 root = TemperatureConverter()
 root.mainloop()
