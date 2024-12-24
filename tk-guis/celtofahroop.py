d3081212d9509a44fa69b2a7384719b7e8d8214b
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-24 09:38:29 +0100

    were getting somewhere

--- tk-guis/celtofahroop.py
+++ tk-guis/celtofahroop.py
@@ -18,17 +18,15 @@ class UserInputFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
 
-        self.user_input = tk.StringVar()
         self.cel_text = tk.StringVar(value="")
         self.converted_text = tk.StringVar(value="")
 
-        label = ttk.Label(self, text="Enter your name: ")
-        entry = ttk.Entry(self, textvariable=self.user_input)
-        button = ttk.Button(self, text="Greet", command=self.greet)
+        label = ttk.Label(self, text="Temperature in Celsius: ")
+        entry = ttk.Entry(self, textvariable=self.cel_text)
+      
 
         label.pack(side="left")
         entry.pack(side="left")
-        button.pack(side="left")
     
     def greet(self):
         print(f"Hello, {self.user_input.get()}!")
