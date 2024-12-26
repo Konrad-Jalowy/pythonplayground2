4448d98ad90c50a7c991aa5306eb4cab67b1ac01
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-26 12:43:44 +0100

    sqlite2

---
+++ python-sql/sqlite2.py
@@ -0,0 +1,15 @@
+import sqlite3
+import os
+
+script_dir = os.path.dirname(__file__)
+os.chdir(script_dir)
+
+db_name = "mydb1.db"
+conn = sqlite3.connect(db_name)
+
+curr = conn.cursor()
+curr.execute('''CREATE TABLE IF NOT EXISTS people (
+                id integer primary key autoincrement,
+                first text,
+                last text,
+                age integer)''')
